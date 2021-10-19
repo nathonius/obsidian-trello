af678f9840f676379a2fd00de3717312ba1c80f7
Nathan Smith <nathan@nathan-smith.org>
2021-10-18 22:55:31 -0400

    fix: correctly bundle nanoid

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,16 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import scss from 'rollup-plugin-scss';
 import { terser } from 'rollup-plugin-terser';
+import replace from '@rollup/plugin-replace';
 
 const isProd = process.env.BUILD === 'production';
-const plugins = [typescript(), nodeResolve({ browser: true }), commonjs(), scss({ output: 'styles.css' })];
+const plugins = [
+  typescript(),
+  nodeResolve({ browser: true }),
+  commonjs(),
+  scss({ output: 'styles.css' }),
+  replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV), preventAssignment: true })
+];
 
 if (isProd) {
   plugins.push(terser());
