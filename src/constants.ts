247b608613dbe7c145bf2f7690d04d226ee3601a
Nathan Smith <nathan@payrollintegrations.com>
2023-11-10 20:33:44 -0500

    doc: add contributing doc and correct version in default settings

--- src/constants.ts
+++ src/constants.ts
@@ -49,7 +49,7 @@ export const DEFAULT_SETTINGS: PluginSettings = {
 
 export const DEFAULT_DATA: PluginData = {
   settings: DEFAULT_SETTINGS,
-  version: '1.6.0',
+  version: '2.0.0',
   firstRun: true,
   connectedCards: {}
 };
