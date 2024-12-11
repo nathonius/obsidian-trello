23962bf62353dfb5b26f348806b988d7de01297e
Chris Chinchilla <chris@chrischinchilla.com>
2024-12-11 17:03:13 +0100

    Revert unneeded commit

--- src/constants.ts
+++ src/constants.ts
@@ -51,7 +51,7 @@ export const DEFAULT_SETTINGS: PluginSettings = {
 
 export const DEFAULT_DATA: PluginData = {
   settings: DEFAULT_SETTINGS,
-  version: '2.3.1',
+  version: '2.3.2',
   firstRun: true,
   connectedCards: {}
 };
